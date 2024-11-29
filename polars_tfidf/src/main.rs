a40c503a47bc0ea7407b8198fdb21a1844d88859
jdm365 <jdm365@georgetown.edu>
2024-11-28 20:13:36 -0700

    Work

--- polars_tfidf/src/main.rs
+++ polars_tfidf/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use polars::prelude::*;
-use sprs::{CsMat, TriMat, CsMatView};
+// use sprs::{CsMat, TriMat, CsMatView};
 
 
 #[allow(dead_code)]
@@ -17,7 +17,6 @@ fn get_lf_columns(lf: &LazyFrame) -> Result<Vec<String>, PolarsError> {
 struct TFToken {
     tf: u32,
     term_id: u32,
-    doc_id: u32,
 }
 
 struct CSRMatrix<T> {
@@ -31,83 +30,34 @@ struct CSRMatrix<T> {
     row_start_pos: Vec<u64>,
 }
 
-#[derive(Clone)]
-struct Tokens {
-    tf: Vec<u32>,
-    term_id: Vec<u32>,
-    doc_id: Vec<u32>,
-}
-
-#[allow(dead_code)]
-impl Tokens {
-    #[inline]
-    fn push(&mut self, tf: u32, term_id: u32, doc_id: u32) {
-        self.tf.push(tf);
-        self.term_id.push(term_id);
-        self.doc_id.push(doc_id);
-    }
-
-    #[inline]
-    fn push_token(&mut self, token: &TFToken) {
-        self.tf.push(token.tf);
-        self.term_id.push(token.term_id);
-        self.doc_id.push(token.doc_id);
-    }
-
-    #[inline]
-    fn get(&self, idx: usize) -> (u32, u32, u32) {
-        (self.tf[idx], self.term_id[idx], self.doc_id[idx])
-    }
-
-    #[inline]
-    fn len(&self) -> usize {
-        self.tf.len()
-    }
-
-    fn new() -> Self {
-        Tokens {
-            tf: Vec::new(),
-            term_id: Vec::new(),
-            doc_id: Vec::new(),
-        }
-    }
-}
-
-#[allow(dead_code)]
-#[derive(Clone)]
-struct TfidfVectorizer {
+struct TfidfVectorizer<T> {
     vocab: HashMap<String, u32>,
     dfs: Vec<u32>,
-    tokens: Tokens,
+    csr_mat: CSRMatrix<T>,
 }
 
 
-fn fit_tfidf(text_series: &Column) -> Result<TfidfVectorizer, PolarsError> {
-    let mut vectorizer = TfidfVectorizer {
+fn fit_tfidf(text_series: &Column) -> Result<TfidfVectorizer<f32>, PolarsError> {
+    let mut vectorizer: TfidfVectorizer<f32> = TfidfVectorizer {
         vocab: HashMap::new(),
         dfs: Vec::new(),
-        tokens: Tokens::new(),
+        csr_mat: CSRMatrix {
+            values: Vec::new(),
+            col_idxs: Vec::new(),
+            row_start_pos: Vec::new(),
+        },
     };
-
     let count = text_series.len();
     vectorizer.dfs.reserve(count);
-
-    // TODO: Calc indptrs through as cumsum of dfs
-    // data: tfidfs
-    // indices: term_ids
-    let mut csr_matrix: CSRMatrix<f32> = CSRMatrix {
-        values: Vec::new(),
-        col_idxs: Vec::new(),
-        row_start_pos: Vec::new(),
-    };
 
     let mut idx: usize = 0;
     let mut doc_tokens: Vec<TFToken> = Vec::new();
     text_series.str()?.into_iter().for_each(|v: Option<&str>| {
-        let _v = match v {
+        let _v: &str = match v {
             Some(v) => v,
             None => { idx += 1; return; },
         };
+
         doc_tokens.clear();
         for token in _v.split_whitespace() {
             if let Some(&term_id) = vectorizer.vocab.get(token) {
@@ -118,7 +68,6 @@ fn fit_tfidf(text_series: &Column) -> Result<TfidfVectorizer, PolarsError> {
                         doc_tokens.push(TFToken{
                             tf: 1,
                             term_id,
-                            doc_id: idx as u32,
                         });
                     },
                 }
@@ -127,36 +76,27 @@ fn fit_tfidf(text_series: &Column) -> Result<TfidfVectorizer, PolarsError> {
                 doc_tokens.push(TFToken{
                     tf: 1,
                     term_id: vectorizer.dfs.len() as u32,
-                    doc_id: idx as u32,
                 });
                 vectorizer.dfs.push(1);
             }
         }
 
+        vectorizer.csr_mat.row_start_pos.push(vectorizer.csr_mat.values.len() as u64);
+
         for token in doc_tokens.iter() {
-            vectorizer.tokens.push_token(token);
+            vectorizer.csr_mat.values.push(token.tf as f32);
+            vectorizer.csr_mat.col_idxs.push(token.term_id);
         }
 
         idx += 1;
     });
 
     let start_time = std::time::Instant::now();
-
-    let mut trimat: TriMat<f32> = TriMat::new((count, vectorizer.vocab.len()));
-    for i in 0..vectorizer.tokens.len() {
-        let (tf, term_id, doc_id) = vectorizer.tokens.get(i);
-
-        let tfidf = tf as f32 * (count as f32 / vectorizer.dfs[term_id as usize] as f32).ln();
-        trimat.add_triplet(doc_id as usize, term_id as usize, tfidf);
+    for (idx, v) in vectorizer.csr_mat.values.iter_mut().enumerate() {
+        let df = vectorizer.dfs[vectorizer.csr_mat.col_idxs[idx] as usize];
+        let idf = (count as f32/ (1 + df) as f32).ln();
+        *v *= idf;
     };
-
-    // let cs_mat_view = CsMatView::new(
-        // (count, vectorizer.vocab.len()),
-        // vectorizer.tokens.tf.as_slice(),
-        // vectorizer.tokens.term_id.as_slice(),
-        // vectorizer.tokens.doc_id.as_slice(),
-    // );
-
     let end_time = start_time.elapsed();
     println!("SPRS construction time: {:?}", end_time);
     Ok(vectorizer)
